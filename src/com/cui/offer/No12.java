b418eb095a96e4cf93e5a118953e787eaf5e2645
GEVIN <320521062@qq.com>
2021-03-29 23:20:10 +0800

    p94

---
+++ src/com/cui/offer/No12.java
@@ -0,0 +1,47 @@
+package com.cui.offer;
+
+//矩阵中的路径
+public class No12 {
+
+    public static void main(String[] args) {
+        char[][] board1 = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word1 = "ABCCED";
+        char[][] board2 = {{'a','b'},{'c','d'}};
+        String word2 = "abcd";
+        System.out.println(exist(board1, word1));
+        System.out.println(exist(board2, word2));
+    }
+
+    public static boolean exist(char[][] board, String word) {
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == word.charAt(0)) {        //寻找矩阵中等于字符串第一个字符的元素作为遍历起点
+                    if (hasPath(board, word.toCharArray(), i, j, 0))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasPath(char[][] board, char[] word, int i, int j, int wordIndex) {
+
+        if(i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1 || board[i][j] != word[wordIndex]) {  //剪枝条件
+            return false;
+        }
+
+        if(wordIndex == word.length - 1) {   //若通过遍历了剪枝条件但字符串已经匹配到最后一个字符，返回true
+            return true;
+        }
+
+        board[i][j] = '\0';   //若当前字符已匹配，设置为空字符表示已访问
+
+        //或连接表示匹配到一条路径即可
+        boolean result = hasPath(board, word, i - 1 , j, wordIndex + 1) || hasPath(board, word, i, j - 1, wordIndex + 1) || hasPath(board, word, i + 1, j, wordIndex + 1) || hasPath(board, word, i, j + 1, wordIndex + 1);
+
+        board[i][j] = word[wordIndex];   //修改回原字符
+
+        return result;
+    }
+}
---
+++ src/com/cui/offer/No13.java
@@ -0,0 +1,62 @@
+package com.cui.offer;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+//机器人的运动范围
+public class No13 {
+
+    public static void main(String[] args) {
+        System.out.println(movingCountDFS(2, 3, 1));
+        System.out.println(movingCountDFS(3, 1, 0));
+        System.out.println(movingCountBFS(2, 3, 1));
+        System.out.println(movingCountBFS(3, 1, 0));
+    }
+    
+    //解法一：DFS+剪枝
+    public static int movingCountDFS(int m, int n, int k) {
+
+        boolean[][] visited = new boolean[m][n];
+        return movingCountDFSCore(visited, 0, 0, k);
+    }
+
+    public static int movingCountDFSCore(boolean[][] visited, int m, int n, int k) {
+        if(m > visited.length - 1 || n > visited[0].length - 1 || getDigitSum(m) + getDigitSum(n) > k )
+            return 0;
+        if(visited[m][n])
+            return 0;
+        visited[m][n] = true;
+        return 1 + movingCountDFSCore(visited, m + 1, n, k) + movingCountDFSCore(visited, m, n + 1, k);
+    }
+
+    //解法二：BFS+剪枝
+    public static int movingCountBFS(int m, int n, int k) {
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
+        Queue<int[]> queue= new LinkedList<>();
+        queue.offer(new int[] { 0, 0});             //从起点(0,0)开始
+        while(queue.size() > 0) {
+            int[] temp = queue.poll();                //遍历的当前元素
+            int x = temp[0], y = temp[1];             //当前元素的x、y坐标
+            if(x >= m || y >= n || getDigitSum(x) + getDigitSum(y) > k || visited[x][y])  //剪枝条件
+                continue;
+            visited[x][y] = true;
+            count++;
+            queue.add(new int[] { x + 1, y});
+            queue.add(new int[] { x, y + 1});
+        }
+        return count;
+    }
+
+    //求数位和
+    public static int getDigitSum(int number) {
+
+        int sum = 0;
+        while(number > 0) {
+            sum = sum +number % 10;
+            number = number /10;
+        }
+        return sum;
+    }
+}
